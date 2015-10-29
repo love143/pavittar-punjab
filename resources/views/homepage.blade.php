89741ab86cd8735d8514c503ce7021c32cebfade
Lovejit Singh <lovejitdhaliwal143@gmail.com>
2015-10-30 00:16:45 +0530

    new files

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/layout')
+
+@section('content')
+	<h1> this is our pavittar punjab </h1>
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head> 
+	<title> @yield('title') </title>
+	<style>   </style>
+</head>
+	<body> 
+		<h2 class= "heading"> Layout php file </h2>
+		@yield('content')
+	</body>
+</html>
