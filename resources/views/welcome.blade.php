5d7e1098a9c46d4c544cf57f7c43230ca25f5e76
Lovejit Dhaliwal <lovejitdhaliwal143@gmail.com>
2016-07-20 15:08:38 +0530

    add welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Pavittar Punjab</div>
             </div>
         </div>
     </body>
